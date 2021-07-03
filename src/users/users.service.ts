ba0d67746a7c7322cd100bdcf4c71060056a63cf
Ikemefuna Obioha <ikemoobioha@gmail.com>
2021-07-03 04:24:34 +0100

    Added edit and delete user functionalities

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Delete, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Users, UsersDocument } from './entities/users.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -36,5 +37,27 @@ export class UsersService {
   async create(createUser: CreateUserDto) {
     const newUser = new this.userModel(createUser);
     return newUser.save();
+  }
+
+  async edit(id: string, editUser: UpdateUserDto) {
+    const existingUser = await this.userModel.findOneAndUpdate(
+      { _id: id },
+      { $set: editUser },
+      { new: true },
+    );
+    if (!existingUser) {
+      throw new NotFoundException('User not found');
+    }
+    return 'User edited successfully';
+  }
+
+  @Delete(':id')
+  async delete(id: string) {
+    const existingUser = await this.findOneByUserId(parseInt(id, 10));
+    if (!existingUser) {
+      return 'User does not exist and therefore can not be deleted';
+    }
+    await existingUser.remove();
+    return 'Successfully deleted user';
   }
 }
